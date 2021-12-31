cd5063f0712629aa1f0a4894b8c2399b2246b6ad
Duncan Kim <duncankim@bitflow.ai>
2021-12-31 11:32:53 +0900

    Move code to OmniPreSenseOPS242

--- WpfApp/MainWindow.xaml.cs
+++ WpfApp/MainWindow.xaml.cs
@@ -25,44 +25,10 @@ namespace WpfApp
     {
         OmniPreSenseOPS242 speedSensor ;
 
-        // Modifiable parameters for this code (not necessarily the sensor's profile)
-        private static int TARGET_MAX_SPEED_ALLOWED = 150;  // max speed to be tracked; anything faster is ignored
-        private static int TARGET_MIN_SPEED_ALLOWED = 13;   // min speed to be tracked; anything slower is ignored
-        private double IDLE_NOTICE_INTERVAL = 10.0;         // time in secs waiting to, um, take action on idle (in state of "not tracking" only)
-        private double TARGETLESS_MIN_INTERVAL_TIME = 0.75; // grace period for object to track again (hysteresis)
-        // note that a change in direction does not allow for this.  
-        private double MIN_TRACK_TO_ACQUIRED_TIME = 0.1;    // min time in secs that object needs to be tracked for it to be counted
-        private string OPS24X_INFO_QUERY_COMMAND = "??";
-
-        // OPS24x configuration parameters (sent to sensor)
-        private string OPS24X_UNITS_PREF = "UC";            // US for MPH, UK for Km/H, "UC" for cm/s
-        private string OPS24X_SAMPLING_FREQUENCY = "SX";    // 10Ksps
-        private string OPS24X_TRANSMIT_POWER = "PX";        // max power
-        private string OPS24X_MAGNITUDE_MIN = "M>20\n";     // Magnitude must be > this
-        private string OPS24X_DECIMAL_DIGITS = "F0";        // F-zero for no decimal reporting
-        private string OPS24X_BLANKS_PREF = "BZ";           // Blanks pref: send 0's not silence
-        private string OPS24X_LIVE_SPEED = "O1OS";          // OS cancels 9243 mode, enables no-delay speeds.  O1 only one speed
-        private string OPS24X_MAX_REPORTABLE = "R<" + TARGET_MAX_SPEED_ALLOWED + "\n";   // Report only < than this speed
-        private string OPS24X_MIN_REPORTABLE = "R>" + TARGET_MIN_SPEED_ALLOWED + "\n";       // Report only > this speed
-        private string OPS24X_BIDIRECTIONAL = "R|";
-        private string OPS24X_INBOUND_ONLY = "R+";
-        private string OPS24X_OUTBOUND_ONLY = "R|";
-        private string OPS24X_DIRECTION_PREF = "R|";        // OPS24X_BIDIRECTIONAL / OPS24X_INBOUND_ONLY;
-
-        // These are for lab development only, so hand-waves are usable
-        // private string OPS24X_UNITS_PREF = "UC";  // "UC" for cm/s
-        // private int TARGET_MAX_SPEED_ALLOWED = 150;
-        // private string OPS24X_DIRECTION_PREF = OPS24X_INBOUND_ONLY;
-        // remove them when moving to actual vehicle testing.
-
-        private System.IO.Ports.SerialPort serialPort1;
-
         public MainWindow()
         {
-            this.serialPort1 = new System.IO.Ports.SerialPort(new System.ComponentModel.Container());
             InitializeComponent();
             speedSensor = new OmniPreSenseOPS242() ;
-            ComponentDispatcher.ThreadIdle += new System.EventHandler( thread_Idle ) ;
         }
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
@@ -80,80 +46,12 @@ namespace WpfApp
             {
                 return;
             }
-
-            if (serialPort1.IsOpen)
-            {
-                serialPort1.Close();
-                serialPort1.PortName = comboBox1.Items[comboBox1.SelectedIndex].ToString();
-            }
-            else
-            {
-                serialPort1.PortName = comboBox1.Items[comboBox1.SelectedIndex].ToString();
-                // baudrate = 115200
-                // parity = serial.PARITY_NONE
-                // stopbits = serial.STOPBITS_ONE
-                // bytesize = serial.EIGHTBITS
-                // timeout = 1
-                // writeTimeout = 2
-                serialPort1.Open();
-
-                SendMessage(OPS24X_SAMPLING_FREQUENCY);
-                SendMessage(OPS24X_TRANSMIT_POWER);
-                SendMessage(OPS24X_MAGNITUDE_MIN);
-                SendMessage(OPS24X_DECIMAL_DIGITS);
-                SendMessage(OPS24X_MIN_REPORTABLE);
-                SendMessage(OPS24X_MAX_REPORTABLE);
-                SendMessage(OPS24X_UNITS_PREF);
-                SendMessage(OPS24X_BLANKS_PREF);
-                SendMessage(OPS24X_LIVE_SPEED);
-                SendMessage(OPS24X_INBOUND_ONLY);
-                
-                readingVelocity = ReadVelocity() ;
-            }
+            speedSensor.ConnectUsb( comboBox1.Items[comboBox1.SelectedIndex].ToString() );
         }
 
         private void DisconnectUsb(object sender, RoutedEventArgs e)
         {
-            serialPort1.Close();
-        }
-
-        private void SendMessage(string message)
-        {
-            // string은 Encoding을 통해 byte[]로 변환
-            // System.Text.Encoding.Unicode.GetBytes (16바이트) / System.Text.Encoding.UTF8.GetBytes (12바이트)
-            byte[] utf8bytes = System.Text.Encoding.UTF8.GetBytes(message);
-            serialPort1.Write(utf8bytes, 0, utf8bytes.Length);
-            ReceiveMessage();
-        }
-
-        private string ReceiveMessage()
-        {
-            string recvMsg = serialPort1.ReadLine();
-            return recvMsg;
-        }
-
-        Task<string> readingVelocity ;
-
-        async Task<string> ReadVelocity()
-        {
-            return await Task.Run( ()=>serialPort1.ReadLine() ) ;
-        }
-
-        void FetchOutVelocity() {
-            if( null == readingVelocity ) {
-                return ;
-            }
-            if( readingVelocity.IsCompleted ) {
-                var text = readingVelocity.Result ;
-                if( text.IndexOf('{') == -1 ) {
-                    txtVelocity.Text = text ;
-                }
-                readingVelocity = ReadVelocity() ;
-            }
-        }
-
-        void thread_Idle( object sender, EventArgs e ) {
-            FetchOutVelocity();
+            speedSensor.DisconnectUsb() ;
         }
     }
 }
--- WpfApp/OmniPreSenseOPS242.cs
+++ WpfApp/OmniPreSenseOPS242.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,6 +12,124 @@ namespace Bitflow
     {
         public TextBlock Display {
             set ; private get ;
+        }
+
+        // Modifiable parameters for this code (not necessarily the sensor's profile)
+        const    int TARGET_MAX_SPEED_ALLOWED = 150;      // max speed to be tracked; anything faster is ignored
+        const    int TARGET_MIN_SPEED_ALLOWED = 13;       // min speed to be tracked; anything slower is ignored
+        const double IDLE_NOTICE_INTERVAL = 10.0;         // time in secs waiting to, um, take action on idle (in state of "not tracking" only)
+        const double TARGETLESS_MIN_INTERVAL_TIME = 0.75; // grace period for object to track again (hysteresis)
+        // note that a change in direction does not allow for this.  
+        const double MIN_TRACK_TO_ACQUIRED_TIME = 0.1;    // min time in secs that object needs to be tracked for it to be counted
+        const string OPS24X_INFO_QUERY_COMMAND = "??";
+
+        // OPS24x configuration parameters (sent to sensor)
+        const string OPS24X_UNITS_PREF = "UC";            // US for MPH, UK for Km/H, "UC" for cm/s
+        const string OPS24X_SAMPLING_FREQUENCY = "SX";    // 10Ksps
+        const string OPS24X_TRANSMIT_POWER = "PX";        // max power
+        const string OPS24X_MAGNITUDE_MIN = "M>20\n";     // Magnitude must be > this
+        const string OPS24X_DECIMAL_DIGITS = "F0";        // F-zero for no decimal reporting
+        const string OPS24X_BLANKS_PREF = "BZ";           // Blanks pref: send 0's not silence
+        const string OPS24X_LIVE_SPEED = "O1OS";          // OS cancels 9243 mode, enables no-delay speeds.  O1 only one speed
+        readonly static string OPS24X_MAX_REPORTABLE = "R<" + TARGET_MAX_SPEED_ALLOWED + "\n";   // Report only < than this speed
+        readonly static string OPS24X_MIN_REPORTABLE = "R>" + TARGET_MIN_SPEED_ALLOWED + "\n";   // Report only > this speed
+        const string OPS24X_BIDIRECTIONAL = "R|";
+        const string OPS24X_INBOUND_ONLY = "R+";
+        const string OPS24X_OUTBOUND_ONLY = "R|";
+        const string OPS24X_DIRECTION_PREF = "R|";        // OPS24X_BIDIRECTIONAL / OPS24X_INBOUND_ONLY;
+
+        // These are for lab development only, so hand-waves are usable
+        // private string OPS24X_UNITS_PREF = "UC";  // "UC" for cm/s
+        // private int TARGET_MAX_SPEED_ALLOWED = 150;
+        // private string OPS24X_DIRECTION_PREF = OPS24X_INBOUND_ONLY;
+        // remove them when moving to actual vehicle testing.
+
+        System.IO.Ports.SerialPort serialPort;
+
+        public OmniPreSenseOPS242()
+        {
+            serialPort = new System.IO.Ports.SerialPort(new System.ComponentModel.Container());
+        }
+
+        public void ConnectUsb( string portName )
+        {
+            if (serialPort.IsOpen)
+            {
+                serialPort.Close();
+            }
+
+            serialPort.PortName = portName ;
+            // baudrate = 115200
+            // parity = serial.PARITY_NONE
+            // stopbits = serial.STOPBITS_ONE
+            // bytesize = serial.EIGHTBITS
+            // timeout = 1
+            // writeTimeout = 2
+            serialPort.Open();
+
+            SendMessage(OPS24X_SAMPLING_FREQUENCY);
+            SendMessage(OPS24X_TRANSMIT_POWER);
+            SendMessage(OPS24X_MAGNITUDE_MIN);
+            SendMessage(OPS24X_DECIMAL_DIGITS);
+            SendMessage(OPS24X_MIN_REPORTABLE);
+            SendMessage(OPS24X_MAX_REPORTABLE);
+            SendMessage(OPS24X_UNITS_PREF);
+            SendMessage(OPS24X_BLANKS_PREF);
+            SendMessage(OPS24X_LIVE_SPEED);
+            SendMessage(OPS24X_INBOUND_ONLY);
+
+            FetchOutVelocity() ;
+        }
+
+        public void DisconnectUsb()
+        {
+            serialPort.Close();
+        }
+
+        void SendMessage(string message)
+        {
+            // string은 Encoding을 통해 byte[]로 변환
+            // System.Text.Encoding.Unicode.GetBytes (16바이트) / System.Text.Encoding.UTF8.GetBytes (12바이트)
+            byte[] utf8bytes = System.Text.Encoding.UTF8.GetBytes(message);
+            serialPort.Write(utf8bytes, 0, utf8bytes.Length);
+            ReceiveMessage();
+        }
+
+        string ReceiveMessage()
+        {
+            string recvMsg = serialPort.ReadLine();
+            return recvMsg;
+        }
+
+        async void FetchOutVelocity()
+        {
+            string contents ;
+            try {
+                contents = await Task.Run( ()=>serialPort.ReadExisting() ) ;
+            }
+            catch {
+                Display.Text = "XXX" ;
+                return ;
+            }
+            double? velocity = null ;
+            using( StringReader sr = new StringReader(contents) ) {
+                string line ;
+                while( ( line=sr.ReadLine() ) != null ) {
+                    if( line.IndexOf('{') >= 0 ) {
+                        continue ;
+                    }
+                    try {
+                        velocity = double.Parse( line ) ;
+                    }
+                    catch {
+                        continue ;
+                    }
+                }
+            }
+            if( velocity != null ) {
+                Display.Text = velocity?.ToString("#0.##") ;
+            }
+            FetchOutVelocity() ;
         }
     }
 }
