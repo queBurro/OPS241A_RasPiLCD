342eaa051aa899c4fd142519190995647200c2e5
tim b <llcoolt@gmail.com>
2024-03-03 16:43:08 +0000

    add console app

---
+++ ConsoleAppSpeed/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using BitflowUtils;
+
+Console.WriteLine("Hello, speed testers!");
+
+OmniPreSenseOPS242 speedSensor;
+
+speedSensor = new OmniPreSenseOPS242();
+
+var serialPortName = OmniPreSenseOPS242.DetectSerialPort();
+
+foreach (string com in System.IO.Ports.SerialPort.GetPortNames())
+{
+    Console.WriteLine($"com ports {com}");
+}
+
+var key = Console.ReadKey();
+
+Console.WriteLine( $"You pressed {key.KeyChar}");
+
+
+//speedSensor.Display = txtVelocity.Text;//ref?
+
+//if (comboBox1.SelectedIndex == 0)
+//{
+//    return;
+//}
+//speedSensor.ConnectUsb(comboBox1.Items[comboBox1.SelectedIndex].ToString());
+
+speedSensor.DisconnectUsb();
+
